30068abff4c0ccc8737af2e1d0470e7f07780457
Toan Ong <tong@HDC-M-0G6H040.local>
2017-05-02 13:58:38 -0600

    Initial

---
+++ src/rosita/linkage/tests/FRILLinkageTest.java
@@ -0,0 +1,31 @@
+package rosita.linkage.tests;
+
+import rosita.linkage.FRILLinker;
+
+public class FRILLinkageTest {
+
+	public static void main(String[] args) 
+	{
+		String str_lnk_file = "";
+		
+		for(int i =0; i<args.length; i++){
+			if (args[i].split("\\=")[0].toLowerCase().equals("config")){
+				str_lnk_file = args[i].split("\\=")[1].trim();
+			}
+		}
+		
+		if(str_lnk_file.length()==0){
+			System.out.print("Linkage configuration file is missing!!!");
+			System.exit(0);
+		}else{
+			str_lnk_file = str_lnk_file.replace('\\', '/');
+		}
+		
+		FRILLinker myFRILLinker = new FRILLinker(str_lnk_file);
+		myFRILLinker.link();
+		
+		
+	}
+	
+
+}
